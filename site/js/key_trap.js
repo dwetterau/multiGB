10fbab6109f3d81e35c51da9459dbdc727aca51e
Calvin MacKenzie <calvinm.mackenzie@gmail.com>
2014-03-01 03:17:21 -0600

    ok

--- site/js/key_trap.js
+++ site/js/key_trap.js
@@ -1,10 +1,30 @@
-Mousetrap.bind('left', function() { window.send_move(1); });
-Mousetrap.bind('right', function() { window.send_move(0); });
-Mousetrap.bind('up', function() { window.send_move(2); });
-Mousetrap.bind('down', function() { window.send_move(3); });
-Mousetrap.bind('a', function() { window.send_move(4); });
-Mousetrap.bind('b', function() { window.send_move(5); });;
-Mousetrap.bind('x', function() { window.send_move(4); });
-Mousetrap.bind('z', function() { window.send_move(5); });
-Mousetrap.bind('enter', function() { window.send_move(7); });
-Mousetrap.bind('shift', function() { window.send_move(6); });
+Mousetrap.bind('left', function(e) {
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(1); });
+Mousetrap.bind('right', function(e) {
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(0); });
+Mousetrap.bind('up', function(e) {
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(2); });
+Mousetrap.bind('down', function(e) { 
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(3); });
+Mousetrap.bind('a', function(e) { 
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(4); });
+Mousetrap.bind('b', function(e) { 
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(5); });;
+Mousetrap.bind('x', function(e) { 
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(4); });
+Mousetrap.bind('z', function(e) { 
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(5); });
+Mousetrap.bind('enter', function(e) { 
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(7); });
+Mousetrap.bind('shift', function(e) { 
+	if (e.preventDefault) e.preventDefault();
+	window.send_move(6); });
